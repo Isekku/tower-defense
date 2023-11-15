a926113632a59c144e5d967d6cbda736e18e6141
Dimitri <fernez.dimitri@gmail.com>
2023-11-15 18:00:39 +0100

    tower basique

---
+++ src/game/tower/Tower.java
@@ -0,0 +1,22 @@
+package game.tower;
+
+public abstract class Tower {
+    private int x;
+    private int y;
+    private int range;
+    private int cost;    
+    private int damage;
+    private int pv;
+    private int pvMax;
+
+    public Tower(int x, int y, int range, int cost, int damage, int pv) {
+        this.x = x;
+        this.y = y;
+        this.range = range;
+        this.cost = cost;
+        this.damage = damage;
+        this.pv = pv;
+        this.pvMax = pv;
+    }
+
+}
