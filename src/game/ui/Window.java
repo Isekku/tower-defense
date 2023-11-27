dbb84d25a35e50bb697c6d000efd97273c420192
Dimitri <fernez.dimitri@gmail.com>
2023-11-27 20:23:57 +0100

    Refactor Window class

--- src/game/ui/Window.java
+++ src/game/ui/Window.java
@@ -5,16 +5,18 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class Window extends JFrame{
+    private Button button;
+
     public Window(String title) {
-        Frame gameFrame = new Frame(title);
-        Button button = new Button("jeu dans la console");
-        gameFrame.add(button);
+        super(title);
+        button = new Button("jeu dans la console");
+        this.add(button);
 
 
-        gameFrame.setSize(400, 400);
-        gameFrame.setVisible(true);
+        this.setSize(400, 400);
+        this.setVisible(true);
 
-        gameFrame.addWindowListener(new WindowAdapter() {
+        this.addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent windowEvent) {
                 System.exit(0);
             }
