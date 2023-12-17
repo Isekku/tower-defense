b69771861f64afc99a42f7702cb6a62b643830e6
Dimitri <fernez.dimitri@gmail.com>
2023-12-17 23:00:17 +0100

    c'est pas beau mais ça marche

--- src/game/ui/Vue/ChooseVersion.java
+++ src/game/ui/Vue/ChooseVersion.java
@@ -10,12 +10,17 @@ public class ChooseVersion extends JFrame implements State {
     protected JPanel chooseVersionPanel;
         protected JPanel sliderPanel;
             protected JSlider slider;
-            protected JLabel sliderLabel;
         protected JPanel buttonPanel;
-            protected JLabel buttonLabel;
-            protected JButton cheminButton;
-            protected JButton decorButton;
+            protected JPanel tabPanel;
+                protected JLabel buttonLabel;
+                protected JButton cheminButton;
+                protected JButton decorButton;
+            protected JPanel modePanel;
+                protected JLabel buttonLabel2;
+                protected JButton normalButton;
+                protected JButton marathonButton;
         protected JButton play;
+
 
     private ChooseVersionController controller;
     private static final ChooseVersion instance = new ChooseVersion();
@@ -32,7 +37,7 @@ public class ChooseVersion extends JFrame implements State {
         // niveau de difficulté (facile, moyen ou difficile) | slider
         // tableau de jeu (chemins ou décors différents)     | 2 boutons
         // le mode de jeu (normal ou marathon)               | 2 boutons
-
+        Font font = new Font("Comic Sans MS", Font.BOLD, 20);
         // ajout du slider
         JPanel sliderPanel = new JPanel(new GridLayout());
             JSlider slider = new JSlider(JSlider.HORIZONTAL, 0, 2, 0);
@@ -44,29 +49,35 @@ public class ChooseVersion extends JFrame implements State {
                 // slider label
                 JLabel sliderLabel = new JLabel("Difficulté");
                 sliderLabel.setHorizontalAlignment(SwingConstants.CENTER);
-                sliderLabel.setFont(new Font("Comic Sans MS", Font.BOLD, 20));
+                sliderLabel.setFont(font);
                 sliderLabel.setForeground(Color.BLACK);
                 
             sliderPanel.add(sliderLabel, BorderLayout.NORTH);
             sliderPanel.add(slider, BorderLayout.CENTER);
-
         // ajout des boutons
         JPanel buttonPanel = new JPanel(new BorderLayout());
-            JPanel tabPanel = new JPanel(new GridLayout(2, 1));
+            JPanel tabPanel = new JPanel(new GridLayout());
                 JLabel buttonLabel = new JLabel("Choix du tableau");
                 JButton cheminButton = new JButton("Chemin");
                 JButton decorButton = new JButton("Décor");
-            JPanel modePanel = new JPanel(new GridLayout(2, 1));
+                buttonLabel.setFont(font);
+            tabPanel.add(buttonLabel);
+            tabPanel.add(cheminButton);
+            tabPanel.add(decorButton);
+
+            JPanel modePanel = new JPanel(new GridLayout());
                 JLabel buttonLabel2 = new JLabel("Choix du mode de jeu");
                 JButton normalButton = new JButton("Normal");
                 JButton marathonButton = new JButton("Marathon");
+                buttonLabel2.setFont(font);
+            modePanel.add(buttonLabel2);
+            modePanel.add(normalButton);
+            modePanel.add(marathonButton);
+                
 
-            buttonPanel.add(buttonLabel);
-            buttonPanel.add(cheminButton);
-            buttonPanel.add(decorButton);
-            buttonPanel.add(buttonLabel2);
-            buttonPanel.add(normalButton);
-            buttonPanel.add(marathonButton);
+        buttonPanel.add(tabPanel, BorderLayout.NORTH);
+        buttonPanel.add(modePanel, BorderLayout.CENTER);
+
 
         JButton play = new JButton("Jouer");
 
