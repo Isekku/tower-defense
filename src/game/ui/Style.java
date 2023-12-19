fc3d4eecea578f82480e9ad559304bc5374958dd
Sekou Cissé <sekoucisse.pr@gmail.com>
2023-12-19 20:41:09 +0100

    Finalisation des emplacements des Components.

--- src/game/ui/Style.java
+++ src/game/ui/Style.java
@@ -10,6 +10,7 @@ import java.awt.*;
 public class Style {
     public static Font titleFont = new Font("Comics Sans MS", Font.BOLD, 60);
     public static Font buttonFont = new Font("Comics Sans MS", Font.BOLD, 20);
+    public static Font descriptionFont = new Font("Comics Sans MS", Font.BOLD, 40);
     public static Font littleButtonFont = new Font("Comics Sans MS", Font.BOLD, 12);
     public static Dimension buttonDimension = new Dimension(300, 70);
     public static Dimension littleButtonDimension = new Dimension(200, 50);
--- src/game/ui/Vue/ChooseMap.java
+++ src/game/ui/Vue/ChooseMap.java
@@ -5,7 +5,9 @@ import game.ui.Style;
 
 import javax.swing.*;
 import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
+import java.util.concurrent.Flow;
 
 public class ChooseMap extends JFrame implements State {
     protected JPanel chooseMapPanel;
@@ -15,41 +17,91 @@ public class ChooseMap extends JFrame implements State {
 
     public ChooseMap(){
         controller = new ChooseMapController(this);
-        chooseMapPanel = new JPanel(new GridLayout(3, 1));
+        chooseMapPanel = new JPanel(new GridLayout(2, 1));
     }
 
     //Les Panel et les Components besoins pour l'affichage :
     JPanel difficultyPanel = new JPanel();
-        JLabel difficultyText = new JLabel("Difficulté : ");
-        JSlider difficultySlider = new JSlider();
+        JPanel difficultyAndDescriptionLabel = new JPanel();
+            JLabel difficultyText = new JLabel("Difficulté : ");
+            JSlider difficultySlider = new JSlider();
 
     JPanel chooseTextPanel = new JPanel();
         JLabel chooseText = new JLabel("Choix de Map :");
 
     JPanel mapChoicePanel = new JPanel();
-        JPanel map1Panel = new JPanel();
-            JLabel map1Image = new JLabel();
-            JLabel map1Title = new JLabel("Plaine des Vertus");
 
-        JPanel map2Panel = new JPanel();
-            JLabel map2Image = new JLabel();
-            JLabel map2Title = new JLabel("Voie Royale");
+        JPanel mapPanel = new JPanel();
+            JPanel map1Panel = new JPanel();
+                JLabel map1Image = new JLabel();
+                JLabel map1Title = new JLabel("Plaine des Vertus");
+
+            JPanel map2Panel = new JPanel();
+                JLabel map2Image = new JLabel();
+                JLabel map2Title = new JLabel("Voie Royale");
 
     public void enterState(){
-        difficultyPanel.setLayout(new BoxLayout(difficultyPanel, BoxLayout.X_AXIS));
-            difficultyText.setFont(Style.buttonFont);
+        difficultyPanel.setLayout(new BorderLayout());
+            difficultyAndDescriptionLabel.setLayout(new FlowLayout(FlowLayout.CENTER));
+                difficultyText.setFont(Style.descriptionFont);
+                setSlider(difficultySlider);
 
-            difficultySlider.setMinimum(0);
-            difficultySlider.setMaximum(2);
-            difficultySlider.setValue(0);
+                difficultyAndDescriptionLabel.add(difficultyText);
+                difficultyAndDescriptionLabel.add(difficultySlider);
 
-            difficultyPanel.add(difficultyText);
-            difficultyPanel.add(difficultySlider);
 
-        chooseMapPanel.add(difficultyPanel);
+
+            difficultyPanel.add(difficultyAndDescriptionLabel, BorderLayout.CENTER);
+
+
+        mapChoicePanel.setLayout(new BorderLayout());
+            chooseTextPanel.setLayout(new BorderLayout());
+                chooseText.setFont(Style.descriptionFont);
+                chooseText.setHorizontalAlignment(SwingConstants.CENTER);
+
+                chooseTextPanel.add(chooseText, BorderLayout.CENTER);
+
+
+            mapPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 200, 130));
+                map1Panel.setLayout(new BoxLayout(map1Panel, BoxLayout.Y_AXIS));
+                map1Panel.setAlignmentX(Component.CENTER_ALIGNMENT);
+                    map1Image.setText("Image de plaine");
+                    map1Title.setText("Plaine des Vertus");
+
+                    map1Panel.add(map1Image);
+                    map1Panel.add(map1Title);
+
+                    mapPanel.add(map1Panel);
+
+                map2Panel.setLayout(new BoxLayout(map2Panel, BoxLayout.Y_AXIS));
+                    map2Image.setText("Image de désert");
+                    map2Title.setText("Voie Royale");
+
+                    map2Panel.add(map2Image);
+                    map2Panel.add(map2Title);
+
+                    mapPanel.add(map2Panel);
+
+            mapChoicePanel.add(chooseTextPanel, BorderLayout.NORTH);
+            mapChoicePanel.add(mapPanel, BorderLayout.CENTER);
+
+
+        chooseMapPanel.add(difficultyPanel, BorderLayout.NORTH);
+        chooseMapPanel.add(mapChoicePanel, BorderLayout.CENTER);
     }
 
     public void quitState(){}
+
+    public void setSlider(JSlider slider){
+        slider.setMajorTickSpacing(1);
+        slider.setPaintTicks(true);
+        slider.setPaintLabels(true);
+        slider.setSnapToTicks(true);
+        slider.setMinimum(0);
+        slider.setMaximum(2);
+        slider.setValue(0);
+        slider.setPreferredSize(new Dimension(300, 100));
+    }
 
     public static ChooseMap getInstance(){
         return instance;
