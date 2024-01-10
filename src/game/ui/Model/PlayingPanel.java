2e2f3d7a61685ef8bddbde00084fa8fcd0218f5e
Sekou Cissé <sekoucisse.pr@gmail.com>
2024-01-10 19:22:01 +0100

    Suppression main test de PlayingPanel

--- src/game/ui/Model/PlayingPanel.java
+++ src/game/ui/Model/PlayingPanel.java
@@ -58,43 +58,4 @@ public class PlayingPanel extends JPanel{
     public void addEntity(Entity entity){
         entityOnPanel.add(entity);
     }
-
-    public static void main(String[] args) {
-        JFrame frame = new JFrame("Playing Panel");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(800, 800);
-
-        Entity mob = new WeakMob(new Coordinates(0, 10, 0.45f));
-
-        PlayingPanel playingPanel = new PlayingPanel();
-        JPanel gridPanel = new JPanel(new GridLayout(5, 1));
-        PlayingPanel[] plateau = new PlayingPanel[5];
-        for(int i = 0; i < plateau.length; i++){
-            plateau[i] = new PlayingPanel();
-            plateau[i].setBorder(Style.compound);
-            gridPanel.add(plateau[i]);
-        }
-
-        frame.add(gridPanel);
-        frame.setVisible(true);
-
-        Coordinates c = new Coordinates(10f, plateau[0].getWidth(), 0.45f);
-        Entity entity = new NormalMob(c);
-        plateau[0].addEntity(entity);
-
-        new Timer(100, new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                c.moveLeft();
-            }
-        }).start();
-
-        try{
-            Thread.sleep(10000);
-            playingPanel.addEntity(new StrongerMob(new Coordinates(10f, playingPanel.getWidth(), 0.45f)));
-        }
-        catch (InterruptedException e){
-            e.printStackTrace();
-        }
-    }
 }
